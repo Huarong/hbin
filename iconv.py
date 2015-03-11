960d067366db6d83bbffcec73f2800337b1b2de5
huohuarong <huohuarong@baidu.com>
2015-03-11 11:11:29 +0800

    add iconv.py

---
+++ iconv.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import sys
+import codecs
+import argparse
+
+
+def parse_cmd_args():
+    parser = argparse.ArgumentParser(description='tansfer encoding. Ignore errors. Output to stdout')
+    parser.add_argument('from_file', help='from file')
+    parser.add_argument('-f', '--from_encode', default=-1, help='from encoding')
+    parser.add_argument('-t', '--to_encode', default=-1, help='to encoding')
+    args = parser.parse_args()
+    return args
+
+
+def run(inpath, from_encode, to_encode):
+    with codecs.open(inpath, encoding=from_encode, errors='ignore') as fi:
+        for line in fi:
+            sys.stdout.write(line.encode(to_encode))
+    return None
+
+
+def main():
+    args = parse_cmd_args()
+    run(args.from_file, args.from_encode, args.to_encode)
+    return None
+
+
+if __name__ == '__main__':
+    main()
