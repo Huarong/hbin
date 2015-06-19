594aedce0176ac3a9683f3e8ad317bd92485e575
huohuarong <huohuarong@baidu.com>
2015-06-19 13:26:05 +0800

    iconv.py input from stdin

--- iconv.py
+++ iconv.py
@@ -9,7 +9,7 @@ import argparse
 
 def parse_cmd_args():
     parser = argparse.ArgumentParser(description='tansfer encoding. Ignore errors. Output to stdout')
-    parser.add_argument('from_file', help='from file')
+    parser.add_argument('-p', '--from_file', default=None, help='from file')
     parser.add_argument('-f', '--from_encode', default=-1, help='from encoding')
     parser.add_argument('-t', '--to_encode', default=-1, help='to encoding')
     args = parser.parse_args()
@@ -17,9 +17,13 @@ def parse_cmd_args():
 
 
 def run(inpath, from_encode, to_encode):
-    with codecs.open(inpath, encoding=from_encode, errors='ignore') as fi:
-        for line in fi:
-            sys.stdout.write(line.encode(to_encode))
+    if inpath:
+        with codecs.open(inpath, encoding=from_encode, errors='ignore') as fi:
+            for line in fi:
+                sys.stdout.write(line.encode(to_encode))
+    else:
+        for line in sys.stdin:
+            sys.stdout.write(line.decode(from_encode).encode(to_encode))
     return None
 
 
